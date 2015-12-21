0d82870333c3e466f6ce24712c157f78a92a5f00
haksung <hacksungeeda@gmail.com>
2015-12-21 11:27:24 +0900

    delete login

--- post_service/urls.py
+++ post_service/urls.py
@@ -3,6 +3,4 @@ from post_service.views import post_list, login, login_validate
 
 urlpatterns = [
 	url(r'^$', post_list),
-	url(r'^login/', login),
-	url(r'^login/validate', login_validate),
 ]
--- post_service/views.py
+++ post_service/views.py
@@ -6,8 +6,7 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.core.context_processors import csrf
 
 from post_service.models import Post
-from post_service.forms import LoginForm
-# testing https://github.com/SoftwareExperiment4/PC.git
+
 # Create your views here.
 def post_list(request):
 	template = get_template('post_list.html')
@@ -28,17 +27,3 @@ def post_list(request):
 	context = Context({'post_list': posts, 'current_page': int(page), 'total_page': range(1, page_data.num_pages + 1)})
 	
 	return HttpResponse(template.render(context))
-	
-def login(request):
-	template = get_template('login_form.html')
-	
-	context = Context({'login_form': LoginForm()})
-	context.update(csrf(request))
-	
-	return HttpResponse(template.render(context))
-	
-def login_validate(request):
-	login_form_data = LoginForm(request)
-	
-	if login_form_data.is_valid():
-		auth.authenticate()
